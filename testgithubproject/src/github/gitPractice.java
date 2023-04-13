2d319dd7b638c3daa8881ae950e47e40838c80ee
HP <HP@DESKTOP-LIQP80D>
2023-04-13 14:04:53 +0530

    added another line

--- testgithubproject/src/github/gitPractice.java
+++ testgithubproject/src/github/gitPractice.java
@@ -4,7 +4,7 @@ public class gitPractice {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Git");
-		System.out.println("Hello Ramya");
+		System.out.println("Hello automation");
 	}
 
 }
