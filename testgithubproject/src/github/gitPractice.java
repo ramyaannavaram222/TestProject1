9c88228aa14ba3b7fcf55fc78965b7bb73be67a8
HP <HP@DESKTOP-LIQP80D>
2023-04-13 14:30:04 +0530

    done

--- testgithubproject/src/github/gitPractice.java
+++ testgithubproject/src/github/gitPractice.java
@@ -4,7 +4,8 @@ public class gitPractice {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Git");
-		System.out.println("Hello automation");
+		System.out.println("Hello Automation");
+		
 	}
 
 }
